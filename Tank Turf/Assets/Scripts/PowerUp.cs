a4dfbf89d5bf52481707f2e8a28cfd9d1b423e56
Spidermig <mabasurto4@gmail.com>
2025-05-01 17:45:59 -0500

    removing unnecessary comments

--- Tank Turf/Assets/Scripts/PowerUp.cs
+++ Tank Turf/Assets/Scripts/PowerUp.cs
@@ -6,7 +6,6 @@ public class PowerUps : MonoBehaviour
 {
     [Header("Inscribed")]
     public Sprite[] powerUpSprites;
-    //public int numPlayerTanks = 3;
 
     // Start is called before the first frame update
     void Start()
