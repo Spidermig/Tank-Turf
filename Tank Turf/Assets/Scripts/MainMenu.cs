696951002f17d10be15998ecb5377e38533423f2
Allan A <allanakk248@gmail.com>
2025-05-06 23:43:27 -0500

    Finished game scene UI(not functionality). connected end screen to home screen. connected tank selection to game scene(selected tank does not appear).

--- Tank Turf/Assets/Scripts/MainMenu.cs
+++ Tank Turf/Assets/Scripts/MainMenu.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+// Goto file(top left corner) > build settings and then youll see all the screens and their acossiated numbers
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
@@ -17,4 +19,20 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
         Debug.Log("Game is quitting...");
     }
+
+    public void endScreen()
+    {
+        // Load the home scene 
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void actualGameScene()
+    {
+        //I think this is where we add what tank to load on the game screen
+
+
+        // Load the game scene
+        SceneManager.LoadSceneAsync(2);
+    }
+    
 }
